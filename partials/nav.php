7646e925e9b0a2e7d25da1f25f0ca8b3060927a5
shailis17 <shailissoni@gmail.com>
2021-11-13 18:36:49 -0500

    bootstrap added

--- partials/nav.php
+++ partials/nav.php
@@ -20,11 +20,18 @@ require(__DIR__."/../lib/functions.php");
 ?>
 
 <!-- include css and js files -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+crossorigin="anonymous"></script>
 <link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
 <!--<script src="helpers.js"></script> -->
 <script src="<?php echo get_url('helpers.js'); ?>"></script>
 
-<nav>
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
