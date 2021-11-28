e953350c4b220d9cbd1e5b5bc7f157fc6a9915af
shailis17 <shailissoni@gmail.com>
2021-11-28 14:36:54 -0500

    create account partially working

--- lib/functions.php
+++ lib/functions.php
@@ -168,84 +168,6 @@ function get_url($dest)
 }
 
 //transactions and account management helper functions
-function get_or_create_account()
-{
-    if (is_logged_in()) {
-        //define data structure first
-        //id is for internal references, account_number is user facing info, and balance will be a cached value of activity
-        $account = ["id" => -1, "account_number" => false, "balance" => 0];
-        //this should always be 0 or 1, but being safe
-        $query = "SELECT id, account, balance from Accounts where user_id = :uid LIMIT 1";
-        $db = getDB();
-        $stmt = $db->prepare($query);
-        try {
-            $stmt->execute([":uid" => get_user_id()]);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if (!$result)  //if account doesn't exist, create it
-            {                
-                $created = false;
-                //we're going to loop here in the off chance that there's a duplicate
-                //it shouldn't be too likely to occur with a length of 12, but it's still worth handling such a scenario
-
-                //you only need to prepare once
-                $query = "INSERT INTO Accounts (account, user_id) VALUES (:an, :uid)";
-                $stmt = $db->prepare($query);
-                $user_id = get_user_id(); //caching a reference
-                $account_number = "";
-                $aid = -1;
-                while (!$created) 
-                {
-                    try 
-                    {
-                        $aid = $db->lastInsertId();
-                        $account_number = str_pad($aid,12,"202", STR_PAD_LEFT);
-                        $stmt->execute([":an" => $account_number, ":uid" => $user_id]);
-                        $created = true; //if we got here it was a success, let's exit
-                    } 
-                    catch (PDOException $e) 
-                    {
-                        $code = se($e->errorInfo, 0, "00000", false);
-                        //if it's a duplicate error, just let the loop happen
-                        //otherwise throw the error since it's likely something looping won't resolve
-                        //and we don't want to get stuck here forever
-                        if (
-                            $code !== "23000"
-                        ) {
-                            throw $e;
-                        }
-                    }
-                }
-                //loop exited, let's assign the new values
-                $account["id"] = $aid;
-                $account["account_number"] = $account_number;
-                $account["balance"] = $result["balance"];
-                $account["account_type"] = $result["account_type"];
-
-                flash("Welcome! Your account has been created successfully", "success");
-                //die(header("Location: my_accounts.php"));
-            } 
-            else 
-            {
-                //$account = $result; //just copy it over
-                $account["id"] = $result["id"];
-                $account["account_number"] = $result["account_number"];
-                $account["balance"] = $result["balance"];
-                $account["account_type"] = $result["account_type"];
-            }
-        } catch (PDOException $e) {
-            flash("Technical error: " . var_export($e->errorInfo, true), "danger");
-        }
-        $_SESSION["user"]["account"] = $account; //storing the account info as a key under the user session
-        if (isset($created) && $created) {
-            refresh_account_balance();
-        }
-        //Note: if there's an error it'll initialize to the "empty" definition around line 161
-
-    } else {
-        flash("You're not logged in", "danger");
-    }
-}
-
 function get_user_account_id()
 {
     if (is_logged_in() && isset($_SESSION["user"]["account"])) {
@@ -271,7 +193,6 @@ function refresh_account_balance()
         $stmt = $db->prepare($query);
         try {
             $stmt->execute([":src" => get_user_account_id()]);
-            get_or_create_account(); //refresh session data
         } catch (PDOException $e) {
             flash("Error refreshing account: " . var_export($e->errorInfo, true), "danger");
         }
