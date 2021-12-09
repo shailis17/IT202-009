a2d05ea006221cde05b78f3917f2d2a3ef7ff7f4
shailis17 <shailissoni@gmail.com>
2021-12-08 20:43:30 -0500

    include partials/pagination.php for pagination navigation

--- public_html/Project/my_accounts.php
+++ public_html/Project/my_accounts.php
@@ -207,6 +207,7 @@ if (isset($_REQUEST["account_id"]))
                         <option value="deposit">Deposit</option>
                         <option value="withdraw">Withdraw</option>
                         <option value="transfer">Transfer</option>
+                        <option value="ext-transfer">External Transfer</option>
                     </select>
                     <span class="input-group-text" id="sort">Sort</span>
                     <select class="form-control" name="sort" aria-label="sort" aria-describedby="sort">
@@ -243,6 +244,7 @@ if (isset($_REQUEST["account_id"]))
                 <?php endforeach; ?>
             <?php endif; ?>
         </table>
+        <?php include(__DIR__ . "/../../partials/pagination.php"); ?>
     <?php endif; ?>
 </div>
 <?php
